567ad4bf36d07f45973b18799c29376c9bfc51f7
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-07-29 19:54:10 -0700

    event-acting

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// // Your code here
+// const game = document.getElementById("dodger");
+// dodger.style.backgroundColor = "#FF69B4";
+
+// function moveDodgerLeft() {
+//   const dodger = document.getElementById("dodger");
+//   let left = parseInt(dodger.style.left);
+//   dodger.style.left = `${left + 1}px`;
+// }
+// function moveDodgerRight() {
+//   const dodger = document.getElementById("dodger");
+//   let right = parseInt(dodger.style.right);
+//   dodger.style.right = `${right + 1}px`;
+// }
+
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  let left = parseInt(dodger.style.left);
+  dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  let left = parseInt(dodger.style.left);
+  dodger.style.left = `${left + 1}px`;
+}
+
+moveDodgerLeft();
+moveDodgerRight();
+
+// let left = parseInt(dodger.style.left);
+// ${left + 1}px
